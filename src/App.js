21b1767beb85a4274ec02d73c0e317cc3d5b9df6
Maxim <110911222+Maxeroz@users.noreply.github.com>
2024-02-19 23:38:48 +0400

    Lorem Ipsum Sctructure

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import data from "./data";
+function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("hello from submit");
+  };
+
+  return (
+    <section className="section-center">
+      <h3>tired of boring lorem ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
+        <button className="btn" type="submit">
+          generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta
+          minus molestiae vel beatae natus eveniet ratione temporibus aperiam
+          harum alias officiis assumenda officia quibusdam deleniti eos
+          cupiditate dolore doloribus!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta
+          minus molestiae vel beatae natus eveniet ratione temporibus aperiam
+          harum alias officiis assumenda officia quibusdam deleniti eos
+          cupiditate dolore doloribus!
+        </p>
+      </article>
+    </section>
+  );
+}
+
+export default App;
